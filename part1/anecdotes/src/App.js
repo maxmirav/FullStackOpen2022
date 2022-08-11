2c8834bbc26419cff23c58bf77678d415d6e5f92
Alexis <miravite.alexis@gmail.com>
2022-08-12 06:45:19 +0800

    1.13 anecdotes step2
    added state for points and determining number of votes per post

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -12,12 +12,23 @@ const App = () => {
   ];
 
     const [selected, setSelected] = useState(0);
+    const [points, setPoints] = useState(Array(anecdotes.length).fill(0));
+
+    const addPoints = () => {
+      const newPoints = {...points}
+      newPoints[selected] += 1;
+      setPoints(newPoints);
+    }
 
   return (
     <div>
       {anecdotes[selected]}
       <br />
+      <span>has {points[selected]} votes</span>
+      <br />
       <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>next anecdote</button>
+      <button onClick={addPoints}>vote</button>
+      
     </div>
   );
 }
