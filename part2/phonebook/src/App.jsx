071b20992eb9fd2d8def4c8ea0f0ebf816d07ad3
Alnux <alexmiravite@gmail.com>
2022-12-17 16:21:46 +0800

    exercise 2.11 phonebook step 6
    data comes from json-server "database"

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,28 @@
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
-import { useState } from 'react'
+
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: '9831283' },
-    { name: 'The Thing', phoneNumber: '4343434' },
-    { name: 'Quack', phoneNumber: '8492' },
-    { name: 'Spiderman', phoneNumber: '1234' },
-    { name: 'Superman', phoneNumber: '23-34-234' },
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [showFilter, setShowFilter] = useState(false)
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log('this is the effect')
+    console.log(persons.length)
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        console.log(persons.length)
+      })
+  }, [])
 
   const filteredPersons = showFilter ? persons.filter(person => person.name.toLowerCase().includes(filter)): persons
 
