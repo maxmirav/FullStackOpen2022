2fe8aca89693c8b73cf93ca448a4b42d3d7ea384
Alnux <alexmiravite@gmail.com>
2022-12-17 13:53:40 +0800

    notes follow along

--- scratchpad/getting-data-from-server/courseinfo-revisit/src/components/Content.jsx
+++
@@ -1,9 +0,0 @@
-import Part from "./Part"
-
-const Content = ({ parts }) => {
-    return(
-        parts.map(part => <Part name={part.name} exercises={part.exercises} key={part.id}/>)
-    )
-}
-
-export default Content
